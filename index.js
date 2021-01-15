6d3570eacee902783305a1d4d1378d9bbe9d3cf4
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2021-01-15 00:35:00 -0500

    Add a few more API functions | Start example code on querying matches

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 // import base libraries
 const api = require("./utils/league-api");
 
-// I'm not going to start building the bot until I complete the api endpoints I need
-api.getTagByName("Carson21", (data, err) => {
+//Rhaenyssx (acc that doesn't have a provisional rank)
+
+api.getAccountIdByName("Rhaenyssx", (id, err) => {
     if (err) return console.log(err);
-    console.log(data);
+
+    api.getMatchesBySummonerId(id, (matches, err) => {
+        if (err) return console.log(err);
+
+        api.getMatchById(matches.matches[0].gameId, (match, err) => {
+            if (err) return console.log(err);
+
+            console.log(match.participantIdentities);
+
+            let participants = new Map();
+
+            for (participant of match.participantIdentites) {
+                participants.set(participant.player.accountId, {
+                    id: participant.participantId,
+                    profileIcon: participant.profileIcon,
+                    platform: participant.currentPlatformId,
+                });
+            }
+
+            for (team of match.teams) {
+                console.log(team);
+            }
+        });
+    });
 });
--- utils/league-api.js
+++ utils/league-api.js
@@ -20,19 +20,20 @@ const region = manifest.api.region;
 let functions = {};
 
 /**
- * Function to return overarching summoner information by their name
+ * Function to return a summoner's tag from their displayname.
  * @param {String} name Name of the summoner to query.
+ * @param {Function} callback callback function containing data
  */
-functions.getTagByName = async (name, callback) => {
+functions.getTagByName = (name, callback) => {
     // retrieve the summoner so we can get the puuid
     api.get(region, "summoner.getBySummonerName", name)
         .then((summoner) => {
-            if (!summoner) return callback(-1);
+            if (!summoner) return callback(null);
 
             // the acc endpoint uses special routing values
             api.get(manifest.api.arv, "account.getByPuuid", summoner.puuid)
                 .then((account) => {
-                    if (!account) return callback(-1);
+                    if (!account) return callback(null);
 
                     // calls back the user's tag.
                     callback(`${account.gameName}#${account.tagLine}`);
@@ -40,6 +41,109 @@ functions.getTagByName = async (name, callback) => {
                 .catch((err) => {
                     callback(null, err);
                 });
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get the matches of a summoner via their ID
+ * @param {String} id SummonerID of the user
+ * @param {Function} callback callback function containing data
+ */
+functions.getMatchesBySummonerId = (id, callback) => {
+    api.get(region, "match.getMatchlist", id)
+        .then((matches) => {
+            if (!matches) return callback(null);
+
+            callback(matches);
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get the puuid of a summoner via their name
+ * @param {String} name Displayname of the summoner.
+ * @param {Function} callback callback function containing data
+ */
+functions.getPuuidByName = (name, callback) => {
+    api.get(region, "summoner.getBySummonerName", name)
+        .then((summoner) => {
+            if (!summoner) return callback(null);
+
+            callback(summoner.puuid);
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get the account id of a summoner via their name
+ * @param {String} name Displayname of the summoner.
+ * @param {Function} callback callback function containing data
+ */
+functions.getAccountIdByName = (name, callback) => {
+    api.get(region, "summoner.getBySummonerName", name)
+        .then((summoner) => {
+            if (!summoner) return callback(null);
+
+            callback(summoner.accountId);
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get match data via its id
+ * @param {String} id ID of the match
+ * @param {Function} callback callback function containing data
+ */
+functions.getMatchById = (id, callback) => {
+    api.get(region, "match.getMatch", id)
+        .then((match) => {
+            if (!match) return callback(null);
+
+            callback(match);
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get the summonerID of a summoner via their name
+ * @param {String} name Displayname of the summoner.
+ * @param {Function} callback callback function containing data
+ */
+functions.getSummonerIdByName = (name, callback) => {
+    api.get(region, "summoner.getBySummonerName", name)
+        .then((summoner) => {
+            if (!summoner) return callback(null);
+
+            console.log(summoner);
+            callback(summoner.id);
+        })
+        .catch((err) => {
+            callback(null, err);
+        });
+};
+
+/**
+ * Function to get league information on a summopner via their summonerID
+ * @param {String} id Id of the summoner
+ * @param {Function} callback callback function containing data
+ */
+functions.getLeaguesBySummonerId = (id, callback) => {
+    api.get(region, "league.getLeagueEntriesForSummoner", id)
+        .then((leagues) => {
+            if (!leagues) return callback(null);
+
+            callback(leagues);
         })
         .catch((err) => {
             callback(null, err);
